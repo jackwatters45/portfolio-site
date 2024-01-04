dfd7fbdfa004cd49294636d8b8f2c2f4647a6b06
Jack Watters <johnwatt@usc.edu>
2024-01-03 19:36:54 -0800

    Add Waldo + Public repos to projects.

--- src/components/Projects/constants/projectsData.tsx
+++ src/components/Projects/constants/projectsData.tsx
@@ -7,6 +7,27 @@ export interface IPortfolioProject {
 
 const projectsData: IPortfolioProject[] = [
 	{
+		name: "Members Only",
+		previewImg:
+			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704250148/members-only.jackwatters.dev__tgbemy.webp",
+		repoUrl: "https://github.com/jackwatters45/express-members-only",
+		liveUrl: "https://members-only.jackwatters.dev/",
+	},
+	{
+		name: "Schmedium Blog Platform",
+		previewImg:
+		"https://res.cloudinary.com/drheg5d7j/image/upload/v1704262356/blog-api-frontend.jackwatters.dev_write_lppdlt.webp",
+		repoUrl: "https://github.com/jackwatters45/blog-api",
+		liveUrl: "https://blog-api-frontend.jackwatters.dev/",
+	},
+	{
+		name: "Notion-inspired Todo List",
+		previewImg:
+		"https://res.cloudinary.com/drheg5d7j/image/upload/v1704250919/www.todo-list.jackwatters.dev_d6bfe314-a75d-4b33-9bdd-edbf90a0f988__yfunny.webp",
+		repoUrl: "https://github.com/jackwatters45/todo-list-react",
+		liveUrl: "https://todo-list.jackwatters.dev/",
+	},
+	{
 		name: "Odin Book",
 		previewImg:
 			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704265614/odin-book.jackwatters.dev_user_6591f3a62e76436d6db8732c_5_cjvt8e.webp",
@@ -14,25 +35,18 @@ const projectsData: IPortfolioProject[] = [
 		liveUrl: "https://odin-book.jackwatters.dev",
 	},
 	{
-		name: "Schmedium Blog Platform",
+		name: "Public Repos Display",
 		previewImg:
-			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704262356/blog-api-frontend.jackwatters.dev_write_lppdlt.webp",
-		repoUrl: "https://github.com/jackwatters45/blog-api",
-		liveUrl: "https://blog-api-frontend.jackwatters.dev/",
+			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704337056/jackwatters45.github.io__1_vtnp0k.webp",
+		repoUrl: "https://github.com/jackwatters45/jackwatters45.github.io",
+		liveUrl: "https://jackwatters45.github.io/",
 	},
 	{
-		name: "Notion-inspired Todo List",
+		name: "Wheres Waldo",
 		previewImg:
-			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704250919/www.todo-list.jackwatters.dev_d6bfe314-a75d-4b33-9bdd-edbf90a0f988__yfunny.webp",
-		repoUrl: "https://github.com/jackwatters45/todo-list-react",
-		liveUrl: "https://todo-list.jackwatters.dev/",
-	},
-	{
-		name: "Members Only",
-		previewImg:
-			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704250148/members-only.jackwatters.dev__tgbemy.webp",
-		repoUrl: "https://github.com/jackwatters45/express-members-only",
-		liveUrl: "https://members-only.jackwatters.dev/",
+			"https://res.cloudinary.com/drheg5d7j/image/upload/v1704336771/jackwatters45.github.io_wheres-waldo-app__wbnfx1.webp",
+		repoUrl: "https://github.com/jackwatters45/wheres-waldo-app",
+		liveUrl: "https://jackwatters45.github.io/wheres-waldo-app/",
 	},
 	{
 		name: "Example Personal Portfolio",
