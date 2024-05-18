a7e8b6dddddbc3c4d521e548e0284962273ea0e9
Jack Watters <johnwatt@usc.edu>
2024-05-17 21:37:01 -0700

    Seo, workers etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind(), mdx()],
-	site: "https://www.jackwatters.dev/",
-	prefetch: true,
+  integrations: [react(), tailwind(), mdx(), sitemap(), partytown()],
+  site: "https://www.jackwatters.dev/",
+  prefetch: true
 });
